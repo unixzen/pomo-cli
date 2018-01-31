963a26be19c121d282476421824c343d320b55cb
Eugene <edemin2@gmail.com>
2018-01-31 22:19:16 +0300

    pomo-cli with colorize and with lock screen utility slock

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # pomo-cli
-Simple pomodoro utility
+Simple pomodoro time management utility. Pomodoro timer seted at 25 minutes as good tradition.
+
+#### Dependencies
+
+* slock
+* python3
+
+For correct work need install slock utility. Official page [slock]|(https://tools.suckless.org/slock/).
+
+#### Usage
+
+Clone repo git@github.com:unixzen/pomo-cli.git. Install slock utility. From pomo-cli directory usage pomo-cli simplest as:
+
+```python3
+python3 pomo-cli.py
+```
+
---
+++ pomo-cli.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import sys
+import time
+from subprocess import call
+
+class bcolors:
+  GREEN = '\033[92m'
+  RED = '\033[91m'
+  ENDC = '\033[0m'
+
+def pomo_duration(duration):
+  print(bcolors.GREEN + "You set duration at", duration, "minutes" + bcolors.ENDC)
+  for time_duration in range(0, duration):
+    print(bcolors.RED + "Remaining...", end=" " + bcolors.ENDC)
+    print(bcolors.RED + " " + str(duration-time_duration) + " " + "\r", end="" + bcolors.ENDC)
+    time.sleep(60)
+  print("\r")
+  print(bcolors.GREEN + "Time to rest!" + bcolors.ENDC)
+# lock screen with utility slock (official page of slock project git clone https://git.suckless.org/slock)
+  call("slock")
+
+# default time is set at 25 minutes
+pomo_duration(25)
+
